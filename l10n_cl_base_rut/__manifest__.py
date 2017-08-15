4a48a6cc9ac40c1e04cdecb2164484f28772ca1b
Daniel Blanco <daniel@blancomartin.cl>
2017-08-15 11:37:44 -0300

    Include OCA as author. Downgrade de version to make first pull request to OCA

--- l10n_cl_base_rut/__manifest__.py
+++ l10n_cl_base_rut/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'author': u'Blanco Martín & Asociados',
+    'author': u'Blanco Martín & Asociados, Odoo Community Association (OCA)',
     'category': 'Localization/Chile',
     'depends': ['base_vat'],
     'description': u'''\n\nIncorporate a field with the RUT (VAT) formatted \
@@ -10,7 +10,7 @@ according chilean customs.\n\n''',
     'name': 'VAT Localization Chile',
     'test': [],
     'update_xml': [],
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.0.0',
     'website': 'https://blancomartin.cl',
     'auto-install': False,
     'active': False
