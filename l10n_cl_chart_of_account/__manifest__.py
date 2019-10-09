a012753c74535c675ee7cd361a740f55dbdfdf61
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-09 09:06:13 -0500

    fix

--- l10n_cl_chart_of_account/__manifest__.py
+++ l10n_cl_chart_of_account/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Chile Localization Chart Account SII",
     "version": "12.0.1.0.0",
-    "author": "Konos, Odoo community Association (OCA)",
+    "author": "Konos, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-chile",
     "category": "Localization/Chile",
     "license": "AGPL-3",
