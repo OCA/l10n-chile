cad26f1d03da643d02f1222b8157a57c49e5b239
Daniel Blanco <daniel@blancomartin.cl>
2019-10-22 16:45:16 -0300

    [FIX] manifest and authors. Add security file

--- l10n_cl_financial_indicators/__manifest__.py
+++ l10n_cl_financial_indicators/__manifest__.py
@@ -1,25 +1,21 @@
 {
-    'name': 'Chilean Financial Indicators',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'license': 'LGPL-3',
-    'author': u'Blanco Martin & Asociados',
-    'website': 'http://blancomartin.cl',
-    'depends': [
-        'base'
+    "name": "Chilean Financial Indicators",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "author": "Blanco Martin & Asociados" "Odoo Community Association (OCA)",
+    "website": "http://blancomartin.cl",
+    "depends": ["base"],
+    "data": [
+        "data/decimal.precision.xml",
+        "data/ir_cron.xml",
+        "data/ir.config_parameter.xml",
+        "security/ir.model.access.csv",
+        "views/update_button.xml",
+        "data/res.currency.csv",
     ],
-    'data': [
-        'data/decimal.precision.xml',
-        'data/ir_cron.xml',
-        'data/ir.config_parameter.xml',
-        'views/update_button.xml',
-        'data/res.currency.csv',
-    ],
-    'init_xml': [
-        'query0.sql',
-        'query1.sql',
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
+    "init_xml": ["query0.sql", "query1.sql"],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
--- l10n_cl_financial_indicators/models/res_currency.py
+++ l10n_cl_financial_indicators/models/res_currency.py
@@ -4,7 +4,8 @@ from odoo.addons import decimal_precision as dp
 
 class CurrencyRate(models.Model):
     _inherit = "res.currency.rate"
-    
+
     rate = fields.Float(
-        digits=dp.get_precision('Currency'),
-        help='The rate of the currency to the currency of rate 1')
+        digits=dp.get_precision("Currency"),
+        help="The rate of the currency to the currency of rate 1",
+    )
