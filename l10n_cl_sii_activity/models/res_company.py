5507018a82b05ec609ca6b2e5795dffc31ca51a1
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-25 22:03:43 -0500

    [ADD] l10n_cl_sii_activity

---
+++ l10n_cl_sii_activity/models/res_company.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    sii_activity_ids = fields.Many2many(
+        "sii.activity", id1="company_id", id2="sii_activity_id",
+        string="SII Activities")
---
+++ l10n_cl_sii_activity/readme/USAGE.rst
@@ -0,0 +1,3 @@
+* Go to Settings > User & Companies > Companies
+* Create or select a company
+* Go to the Activities tab to set them
