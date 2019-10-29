d95e0cdb8b6719842e315984a71448775f84c88b
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-29 14:29:04 -0600

    [ADD] res_partner_email_etd

---
+++ res_partner_email_etd/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Email ETD",
+    "summary": "To receive Electronic Tax Documents",
+    "version": "12.0.1.0.0",
+    "category": "Localization/Chile",
+    "author": "Daniel Santibáñez Polanco, "
+              "Cooperativa OdooCoop, "
+              "Konos, "
+              "Open Source Integrators, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-chile",
+    "license": "AGPL-3",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["nelsonramirezs"],
+}
