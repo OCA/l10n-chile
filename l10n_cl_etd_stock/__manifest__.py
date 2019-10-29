fb94db12ed7585629fac2c1d6f42cdb6ba5d1fff
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-29 08:43:35 -0600

    [ADD] l10n_cl_etd_stock

---
+++ l10n_cl_etd_stock/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Electronic Shipping for Chile",
+    "summary": "Sign your delivery orders and send them to SII.",
+    "version": "12.0.1.0.0",
+    "category": "Localization/Chile",
+    "author": "Daniel Santibáñez Polanco, "
+              "Cooperativa OdooCoop, "
+              "Konos, "
+              "Open Source Integrators, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-chile",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_cl_etd",
+        "delivery",
+        "stock",
+    ],
+    "data": [
+        "data/etd_document.xml",
+        "views/stock_picking.xml",
+        "views/delivery_carrier.xml",
+    ],
+    "application": True,
+    "development_status": "Beta",
+    "maintainers": ["nelsonramirezs"],
+}
---
+++ l10n_cl_etd_stock/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    etd_document,
+    delivery_carrier,
+    stock_picking,
+)
