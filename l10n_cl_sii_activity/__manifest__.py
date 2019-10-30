efcda1237fb3b63e8a4836d4e8c2283ee214fd55
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-29 18:12:15 -0600

    [FIX] l10n_cl_sii_activity

--- l10n_cl_sii_activity/__manifest__.py
+++ l10n_cl_sii_activity/__manifest__.py
@@ -4,8 +4,8 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Company Activities from SII",
-    "summary": "Set the SII Activities on Companies",
+    "name": "Partner Activity from SII",
+    "summary": "Set the SII Activity on Partners",
     "version": "12.0.1.0.0",
     "category": "Localization/Chile",
     "author": "Daniel Santibáñez Polanco, "
