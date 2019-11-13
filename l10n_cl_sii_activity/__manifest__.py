aceeb1c85344018bec59bc43870913fa85c44fb6
Daniel Blanco <daniel@blancomartin.cl>
2019-11-13 11:42:41 -0300

    [FIX] Correct the authorship of sii_activity original work at https://github.com/odoo-chile/l10n_cl_partner_activities/tree/8.0

--- l10n_cl_sii_activity/__manifest__.py
+++ l10n_cl_sii_activity/__manifest__.py
@@ -8,7 +8,9 @@
     "summary": "Set the SII Activity on Partners",
     "version": "12.0.1.0.0",
     "category": "Localization/Chile",
-    "author": "Daniel Santibáñez Polanco, "
+    "author": "Daniel Blanco, "
+              "Blanco Martín & Asociados, "
+              "Daniel Santibáñez Polanco, "
               "Cooperativa OdooCoop, "
               "Konos, "
               "Open Source Integrators, "
