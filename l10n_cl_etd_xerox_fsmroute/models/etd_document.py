64007aedadc3a053d7a68d207c58397ea58f77e2
Daniel Reis <dreis@opensourceintegrators.com>
2020-03-09 11:14:28 +0000

    [FIX] ETD Xerox FSM: no duplication risk, so process all Invoices and Deliveries

--- l10n_cl_etd_xerox_fsmroute/models/etd_document.py
+++ l10n_cl_etd_xerox_fsmroute/models/etd_document.py
@@ -13,21 +13,7 @@ class EtdDocument(models.Model):
 
     def _xerox_get_records(self, company_id, run_date):
 
-        def _is_in_dayroute(rec):
-            """Check if doc is included in a Day Route"""
-            if rec._name == 'account.invoice':
-                return bool(rec.fsm_order_ids.dayroute_id)
-            if rec._name == 'stock.picking':
-                return bool(rec.fsm_order_id.dayroute_id)
-            return False
-
-        def _filter_out_dayroute(rset):
-            """Filter out docs already included in a Day Route"""
-            return rset.filtered(lambda x: not _is_in_dayroute(x))
-
         res = super()._xerox_get_records(company_id, run_date)
-        res = {key: _filter_out_dayroute(rset) for key, rset in res.items()}
-
         next_date = date_utils.add(run_date, days=1)
         res['fsm.route.dayroute'] = self.env["fsm.route.dayroute"].search([
             ("stage_id.is_closed", "=", "True"),
