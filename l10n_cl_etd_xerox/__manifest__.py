2a1b2b420030fffd9342f319fd990fba0d8ed0c2
Daniel Reis <dreis@opensourceintegrators.com>
2020-05-02 16:48:48 +0100

    [REF] etd_xerox: default backend config is now demo data

--- l10n_cl_etd_xerox/__manifest__.py
+++ l10n_cl_etd_xerox/__manifest__.py
@@ -21,11 +21,13 @@
         "fieldservice_vehicle",
     ],
     "data": [
-        "data/backend.acp.csv",
         "data/etd_document_invoice.xml",
         "data/etd_document_stock.xml",
         "data/etd_xerox_cron.xml",
         "views/backend_acp.xml",
         "views/stock_warehouse.xml",
+    ],
+    "demo": [
+        "demo/backend.acp.csv",
     ],
 }
