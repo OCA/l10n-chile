bfc64c08c6d7e9c07784125c2d368c7294a4bde1
Daniel Reis <dreis@opensourceintegrators.com>
2020-05-03 18:00:02 +0100

    [IMP] etd_xerox: Picking Batch lines grouped by Product/UoM

---
+++ l10n_cl_etd_xerox/models/stock_picking_batch.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class PickingBatch(models.Model):
+    _inherit = "stock.picking.batch"
+
+    def get_xerox_lines(self):
+        """
+        Returns  a dictionary with the lines for the Xerox Picking Batch report
+        """
+        lines = {}
+        for line in self.mapped('picking_ids.move_ids_without_package'):
+            key = (line.product_id, line.product_uom)
+            lines.setdefault(key, {'quantity': 0, 'amount': 0})
+            lines[key]['quantity'] += (
+                line.quantity_done or line.product_uom_qty)
+            lines[key]['amount'] += line.sale_line_id.price_subtotal
+        return lines
