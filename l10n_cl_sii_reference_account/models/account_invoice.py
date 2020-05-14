96f4d15f788721dde0a3b261f3c4945bc4b220e9
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-05-14 18:37:46 -0500

    [IMP] l10n_cl_sii_reference_account: Fill in the references on the invoice

--- l10n_cl_sii_reference_account/models/account_invoice.py
+++ l10n_cl_sii_reference_account/models/account_invoice.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 CubicERP
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -12,3 +12,82 @@ class AccountInvoice(models.Model):
     reference_ids = fields.One2many(
         "sii.reference", "invoice_id", readonly=True,
         states={"draft": [("readonly", False)]})
+
+    def prepare_ref_values(self, record=False):
+        res = {}
+        if record:
+            if record._name == 'account.invoice':
+                res.update({
+                    'name': record.number,
+                    'motive': record.name,
+                    'code': "1",
+                    'date': record.date_invoice,
+                    'class_id': record.class_id.id,
+                    'invoice_id': self.id,
+                })
+            elif record._name == 'stock.picking':
+                res.update({
+                    'name': record.name,
+                    'motive': record.name,
+                    'date':
+                        record.date_done and record.date_done.date() or
+                        record.scheduled_date.date(),
+                    'class_id': record.class_id.id,
+                    'invoice_id': self.id,
+                })
+            elif record._name == "sale.order":
+                if record.client_order_ref:
+                    # Customer Purchase Order
+                    res.update({
+                        'name': record.client_order_ref,
+                        'motive': record.name,
+                        'date': record.confirmation_date.date(),
+                        'class_id':
+                            self.env.ref('l10n_cl_sii_reference.dc_oc').id,
+                        'invoice_id': self.id,
+                    })
+            elif record._name == "sale.blanket.order":
+                if record.client_order_ref:
+                    # Customer Purchase Order
+                    res.update({
+                        'name': record.client_order_ref,
+                        'motive': record.name,
+                        'date': record.validity_date,
+                        'class_id': self.env.ref(
+                            'l10n_cl_sii_reference.dc_oc').id,
+                        'invoice_id': self.id,
+                    })
+        return res
+
+    def create_refs_from_sol(self, sol=False):
+        ref = self.env['sii.reference']
+        if sol and sol.order_id.client_order_ref:
+            ref.create(self.prepare_ref_values(sol.order_id))
+        if sol and sol.blanket_order_line:
+            ref.create(self.prepare_ref_values(
+                sol.blanket_order_line.order_id))
+        for picking in sol.order_id.picking_ids.filtered(
+                lambda x:
+                x.class_id is not False and
+                x.picking_type_id.code == 'outgoing'):
+            ref.create(self.prepare_ref_values(picking))
+        return True
+
+    @api.depends('refund_invoice_id', 'invoice_lines')
+    def compute_refs(self):
+        for rec in self:
+            if not rec.reference_ids:
+                ref = self.env['sii.reference']
+                if rec.refund_invoice_id:
+                    ref.create(rec.prepare_ref_values(rec.refund_invoice_id))
+                elif rec.invoice_line_ids:
+                    for line in rec.invoice_line_ids:
+                        for sol in line.sale_line_ids:
+                            rec.create_refs_from_sol(sol)
+
+    @api.multi
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self.filtered(lambda x: x.state == 'draft'):
+            rec.compute_refs()
+        return res
--- l10n_cl_sii_reference_account/models/sii_reference.py
+++ l10n_cl_sii_reference_account/models/sii_reference.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 CubicERP
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SiiReference(models.Model):
@@ -11,3 +11,25 @@ class SiiReference(models.Model):
 
     invoice_id = fields.Many2one("account.invoice", string="Invoice",
                                  ondelete="cascade", index=True, copy=False)
+
+    _sql_constraints = [(
+        "name_class_invoice",
+        "unique(name, class_id, invoice_id)",
+        "The document is already referenced on this invoice!"
+    )]
+
+    @api.model
+    def create(self, vals):
+        res = False
+        if vals.get('name', False) and vals.get('class_id', False) and \
+                vals.get('invoice_id', False):
+            exist = self.search([
+                ('name', '=', vals.get('name')),
+                ('class_id', '=', vals.get('class_id')),
+                ('invoice_id', '=', vals.get('invoice_id')),
+            ])
+            if not exist:
+                res = super().create(vals)
+        else:
+            res = super().create(vals)
+        return res
