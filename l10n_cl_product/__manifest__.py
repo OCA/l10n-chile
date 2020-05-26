71147b4e3f2348c8d35c8dca89602a16fdc0e77f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-05-26 14:40:28 -0500

    [ADD] l10n_cl_product

---
+++ l10n_cl_product/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Electronic Tax Document Code on Product",
+    "summary": "Manage product codes for electronic invoicing",
+    "version": "12.0.1.0.0",
+    "category": "Localization/Chile",
+    "author": "Open Source Integrators, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-chile",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_template.xml",
+    ],
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
+}
